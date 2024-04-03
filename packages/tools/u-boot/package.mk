2caf7f4e8681759d0db65d734a8e020d87795aa2
Rudi Heitbaum <rudi@heitbaum.com>
2024-04-03 10:39:38 +0000

    u-boot: update to 2025.04
    do not build mkeficapsule tool on NXP or Qualcomm (requires gnutls)
    - https://patchwork.ozlabs.org/project/uboot/patch/20240531225110.2622239-2-j-humphreys@ti.com/

--- packages/tools/u-boot/package.mk
+++ packages/tools/u-boot/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="u-boot"
-PKG_VERSION="2024.01"
-PKG_SHA256="b99611f1ed237bf3541bdc8434b68c96a6e05967061f992443cb30aabebef5b3"
+PKG_VERSION="2025.04"
+PKG_SHA256="439d3bef296effd54130be6a731c5b118be7fddd7fcc663ccbc5fb18294d8718"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.denx.de/wiki/U-Boot"
