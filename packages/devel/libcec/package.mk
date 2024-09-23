2d31e282f1a4cdfe99481420a964a76213393531
Rudi Heitbaum <rudi@heitbaum.com>
2024-09-23 11:58:23 +0000

    libcec: update to 7.1.1

--- packages/devel/libcec/package.mk
+++ packages/devel/libcec/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libcec"
-PKG_VERSION="4.0.7"
-PKG_SHA256="bcd92c376993a5721d346edcc09eb17289451f9156b1d1d113c9663c2046315a"
+PKG_VERSION="7.1.1"
+PKG_SHA256="7f7da95a4c1e7160d42ca37a3ac80cf6f389b317e14816949e0fa5e2edf4cc64"
 PKG_LICENSE="GPL"
 PKG_SITE="http://libcec.pulse-eight.com/"
 PKG_URL="https://github.com/Pulse-Eight/libcec/archive/libcec-${PKG_VERSION}.tar.gz"
@@ -13,11 +13,10 @@ PKG_LONGDESC="libCEC is an open-source dual licensed library designed for commun
 
 PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=1 \
                        -DCMAKE_INSTALL_LIBDIR:STRING=lib \
-                       -DCMAKE_INSTALL_LIBDIR_NOARCH:STRING=lib \
                        -DSKIP_PYTHON_WRAPPER=1 \
                        -DHAVE_IMX_API=0 \
-                       -DHAVE_AOCEC_API=0 -DHAVE_AMLOGIC_API=0 \
-                       -DHAVE_GIT_BIN=0 \
+                       -DHAVE_AOCEC_API=0 \
+                       -DGIT_BIN=0 \
                        -DHAVE_RPI_LIB=0"
 
 # libX11 and xrandr to read the sink's EDID, used to determine the PC's HDMI physical address
